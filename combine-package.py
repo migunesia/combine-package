d098f95df649f76770442defd6797766e2421dc1
Maika Kanaka <maika-kanaka@protonmail.com>
2020-05-05 20:52:26 +0700

    combine npm package with files argument

--- combine-package.py
+++ combine-package.py
@@ -1,4 +1,4 @@
-import os.path
+from os import path, system
 import json
 
 import fire
@@ -39,9 +39,9 @@ class CombinePackage:
         # valid & fill param file_list
         if file_list != "":
             # check file_list is exists
-            if os.path.isfile(file_list):
+            if path.isfile(file_list):
                 self.file_list = file_list
-                self.path_file_list = os.path.dirname(os.path.realpath(self.file_list))
+                self.path_file_list = path.dirname(path.realpath(self.file_list))
             else:
                 self.show_error(
                     "<not_yet>",
@@ -55,7 +55,7 @@ class CombinePackage:
             all_exists = True
 
             if all_exists:
-                self.files = files
+                self.files = files.split(",")
             else:
                 self.show_error(
                     "<not_yet>",
@@ -64,12 +64,12 @@ class CombinePackage:
                 return
 
             # valid & fill param combine to
-            if os.path.exists(combine_to):
+            if path.exists(combine_to):
                 self.combine_to = combine_to
             else:
                 self.show_error(
                     "<not_yet>",
-                    ""
+                    "The given path for combine_to is incorrect"
                 )
                 return
 
@@ -110,8 +110,6 @@ class CombinePackage:
                     # check version
                     # comingsoon: get the last version of the package is coming soon
 
-        # npm init
-
         # path file package.json for combine
         if("\\" in self.path_file_list):
             cdn_package_json = self.path_file_list + "\\package.json"
@@ -119,7 +117,14 @@ class CombinePackage:
             cdn_package_json = self.path_file_list + "/package.json"
 
         # read file & replace dependencies
-        r_cdn_package_json = open(cdn_package_json, "r")
+        if path.isfile(cdn_package_json):
+            # open file package.json
+            r_cdn_package_json = open(cdn_package_json, "r")
+        else:
+            # npm init if package.json is not exists
+            system("cd {} && npm init -y".format(self.path_file_list))
+            r_cdn_package_json = open(cdn_package_json, "r")
+
         c_cdn_package_json = json.loads(r_cdn_package_json.read())
         c_cdn_package_json["dependencies"] = dep_combine
         r_cdn_package_json.close()
@@ -130,7 +135,48 @@ class CombinePackage:
         w_cdn_package_json.close()
 
     def _npm_by_param(self):
-        print("Starting combine package by files param ....")
+        print("Starting combine package by file below: ")
+
+        dependencies = []
+        for f in self.files:
+            print(f.strip())
+            file_package_json = open(f.strip(), "r")
+            package_json = json.loads(file_package_json.read())
+            dependencies.append(package_json["dependencies"])
+
+        dep_combine = {}
+        for dd in dependencies:
+            for dname, dver in dd.items():
+                if(dname not in dep_combine):
+                    # if package not exists then append
+                    dep_combine["{}".format(dname)] = "{}".format(dver)
+                # else:
+                    # check version
+                    # comingsoon: get the last version of the package is coming soon
+
+        # path file package.json for combine
+        if("\\" in self.combine_to):
+            cdn_package_json = self.combine_to + "\\package.json"
+        else:
+            cdn_package_json = self.combine_to + "/package.json"
+
+        # read file & replace dependencies
+        if path.isfile(cdn_package_json):
+            # open file package.json
+            r_cdn_package_json = open(cdn_package_json, "r")
+        else:
+            # npm init if package.json is not exists
+            system("cd {} && npm init -y".format(self.combine_to))
+            r_cdn_package_json = open(cdn_package_json, "r")
+
+        c_cdn_package_json = json.loads(r_cdn_package_json.read())
+        c_cdn_package_json["dependencies"] = dep_combine
+        r_cdn_package_json.close()
+
+        # write file
+        w_cdn_package_json = open(cdn_package_json, "w")
+        w_cdn_package_json.write(json.dumps(c_cdn_package_json, indent=4))
+        w_cdn_package_json.close()
 
     def show_error(self, code, text):
         print(colored.red("ERROR {}: {}." . format(code, text)))
